2962a4bde026247fef513a21233d98e19e1df0d2
raysulkobir <raysulkobir97@gmail.com>
2022-09-23 22:22:48 +0600

    update

---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+function Index() {
+    return <div>Index
+      {console.log("hell")}
+    </div>
+  }
+  
+  export default Index
+ 
---
+++ pages/customer/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+function Customer({ customers }) {
+    const data = customers;
+    return(
+        <>
+            hello
+        </> 
+    )
+  }
+  
+  export default Customer
+
+
+ 
+
+  // This function gets called at build time
+ 
+ 
